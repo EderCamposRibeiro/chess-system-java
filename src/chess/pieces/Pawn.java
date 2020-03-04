57553ea4b4afa939d5b3f9a97eb3f4f2245369b7
EderCamposRibeiro <45081914+EderCamposRibeiro@users.noreply.github.com>
2020-03-04 18:58:02 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -57,7 +57,7 @@ public class Pawn extends ChessPiece{
 			}
 			//Para frente duas casas na primeira jogada da peça
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) &&
 					!getBoard().thereIsAPiece(p) && 
 					getBoard().positionExists(p2) && 
